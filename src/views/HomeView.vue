47e02ec8c2d1952d40f76af0a082787dab93f5b6
Jim Sadler <jim@jsdevelopment.tech>
2024-09-08 11:30:58 -0400

    wip

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,10 +2,17 @@
 </script>
 
 <template>
-    <h1>Welcome to the Nassau Community Jazz Band! <v-icon>mdi-information</v-icon></h1>
-  <div class="h-100 w-100"><v-img width="100%" aspect-ratio="16/9" class="mx-auto" cover  src="/src/assets/images/njb_county_fair_full.jpeg" alt="NCJB Logo">
+    <h1>Welcome to the Nassau Community Band <v-icon>mdi-information</v-icon></h1>
+  <!--<v-img-->
+  <!--  src="/DickmanMarcProfile.jpg"-->
+  <!--  class="mt-1"-->
+
+  <!--/>-->
+  <div class="h-100 w-100">
+    <v-img width="100%" aspect-ratio="16/9" class="mx-auto" cover  src="/njb_county_fair_full.jpeg" alt="NCJB Logo">
     <div class="fill-height bottom-gradient"></div>
-  </v-img></div>
+  </v-img>
+  </div>
 </template>
 <style>
 .bottom-gradient {
